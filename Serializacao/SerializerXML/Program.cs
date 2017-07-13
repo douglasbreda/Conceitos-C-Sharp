39609ec71bdfcbf3de1925f4363f1a64d08c0a9d
Douglas <bredadouglas@gmail.com>
2017-07-13 13:11:41 -0300

    first commit

---
+++ Serializacao/SerializerXML/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Library;
+
+namespace SerializerXML
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            User oUser = new User() { Name = "Douglas", Email = "bredadouglas@gmail.com", Identification = "000.000.000-92" };
+
+            XmlSerializer oXmlSerializer = new XmlSerializer( typeof( User ) );
+
+            StreamWriter oStreamWriter = new StreamWriter( "serializedXML.xml" );
+
+            oXmlSerializer.Serialize( oStreamWriter, oUser );
+        }
+    }
+}
