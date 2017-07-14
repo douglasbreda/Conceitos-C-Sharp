f60abf2574d263ab97f8893b33a9f58faab95fcc
Douglas <bredadouglas@gmail.com>
2017-07-14 13:16:29 -0300

    Added serializer JSON

---
+++ Serializacao/DeserializeXML/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using Library;
+
+namespace DeserializeXML
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            StreamReader streamReader = new StreamReader( "..\\..\\..\\Files\\serializedXML.xml" );
+            XmlSerializer serializer = new XmlSerializer( typeof( User ) );
+
+            User user = ( User ) serializer.Deserialize( streamReader );
+
+            Console.WriteLine( $"Usuário: {user.Name}, CPF: {user.Identification}, Email: {user.Email} " );
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Serializacao/SerializerJSON/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Web.Script.Serialization;
+using Library;
+
+namespace SerializerJSON
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            User oUser = new User() { Name = "Douglas", Email = "bredadouglas@gmail.com", Identification = "000.000.000-92" };
+
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+            string sJson = serializer.Serialize( oUser );
+
+            StreamWriter oStreamWriter = new StreamWriter( "..\\..\\..\\Files\\serializedXML.json" );
+            oStreamWriter.Write( sJson );
+            oStreamWriter.Close();
+        }
+    }
+}
